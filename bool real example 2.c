8f0c481e99d7bc36ce879d46de40bc86f3f230d1
phravintreuno@gmail.com <your@email.com>
2026-01-15 15:42:34 +0530

    ADDED BOOL REAL EXAMPLE

---
+++ bool real example 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main () {
+int phravinsage = 21;
+int kavithasage = 43;
+if (phravinsage <= kavithasage){
+printf("phravin is younger than kavitha");
+}else{
+printf("kavitha is older than phravin");
+}
+return 0;
+}
