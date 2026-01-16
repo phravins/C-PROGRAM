21e14f3121283dfecd7a7c621b3818005aa6dda3
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-16 10:40:39 +0530

    ADDED ESCAPE SEQUENCE IN C

---
+++ Escape Sequence in C.c
@@ -0,0 +1,9 @@
+#include <Stdio.h>
+int main () {
+ printf("\"ESCAPE SEQUENCE IN C\"\n");
+ printf("country:\t\capital:\n");
+ printf("India\t\tNew Delhi\n");
+ printf("France\t\tParis\n");
+ printf("Japan\t\tTokyo");
+ return 0;
+}
