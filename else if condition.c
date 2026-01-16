63eb85bc405cc55dac2ca944f89790802e817cc1
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-16 12:30:27 +0530

    ADDED ELSE IF CONDITION

---
+++ else if condition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+int num = 24;
+ if (9 > num){
+  printf("Good morning");
+}else if (12 < num){
+  printf("Good afternoon");
+}else{
+  printf("Good evening");
+}
+ return 0;
+}
