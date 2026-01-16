d9f244c8b694521c85f37b44034c7b56512b30eb
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-16 10:28:22 +0530

    ADDED BOOL REAL EXAMPLE 3

---
+++ bool real example 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main () {
+ bool myage = true;
+ bool votingage = false;
+ if (myage <= votingage){
+ printf("myage is satisfies the voting age");
+ } else {
+ printf("myage is not satisfies the voting age");
+ }
+ return 0;
+}
