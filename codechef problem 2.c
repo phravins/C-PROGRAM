0a814fd3a17db194e43a8e419ebbd2a64dbcc0a5
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-17 19:50:10 +0530

    Update program to display total distance travelled
    Replaced height and weight variables with total distance and character variables.

---
+++ codechef problem 2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    long int total_distance = 360590000;
+    char character = 'k';
+    char character2 = 'm';
+    printf("Total distance travelled: %ld\t%c%c", total_distance,character,character);
+    return 0;
+}
