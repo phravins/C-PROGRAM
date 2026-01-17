a024cdecf60d40ee35ed24d80db3fb111e5cc310
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-17 20:13:17 +0530

    Add flight altitude program in codechef 4.c

---
+++ codechef 4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float average_altitude = 35000.750000;
+    float maximum_altitude = 45367.098765;
+    printf("Average Flight Altitude (float): %f\n", average_altitude);
+    printf("Maximum Flight Altitude (double): %f", maximum_altitude);
+    return 0;
+}
