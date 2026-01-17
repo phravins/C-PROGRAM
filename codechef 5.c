df6f5f7eeace085bba76cbf401e2788c6948cdb7
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-17 20:18:39 +0530

    Add code to display original and discounted prices

---
+++ codechef 5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    double originalPrice = 1678.991233;   
+    float discountedPrice = 1600.0f;     
+    printf("Original Price: %.6lf\n", originalPrice);
+    printf("Discounted Price: %.6f", discountedPrice);
+    return 0;
+}
