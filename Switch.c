2486dc5baa30130094536bf1afd304ac5c2ebbcd
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-18 13:57:37 +0530

    Add switch statement to display day of the week

---
+++ Switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main () {
+int day = 3;
+switch (day) {
+case 1:
+    printf("Today is sunday");
+    break;
+case 2:
+    printf("Today is monday");
+    break;
+case 3:
+    printf("Today is tuesday");
+    break;
+case 4:
+    printf("Today is wednesday");
+    break;
+case 5:
+    printf("Today is thursday");
+    break;
+case 6:
+    printf("Today is friday");
+    break ;
+case 7:
+    printf("Today is saturday");
+    }
+    return 0;
+}
