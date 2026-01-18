8f7878c135725f028340bc5044d112d49f21d613
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-18 13:48:57 +0530

    Add password check in real world problem 3.c

---
+++ real wolrd problem 3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+int num = 12345;
+if (num == 2345){
+    printf("Password is correct");
+    } else {
+    printf("Password is incorrect");
+    }
+    return 0;
+}
