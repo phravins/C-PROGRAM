99cb7408dc032412bde0821941b49d7f1354588e
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-25 12:32:47 +0530

    Create Scanf int.c

---
+++ Scanf int.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <Stdlib.h>
+int main () {
+    int num;
+    printf("Enter your favourite number: ");
+    scanf("%d",&num);
+    printf("Your favourite number is: %d",num);
+    return 0;
+}
