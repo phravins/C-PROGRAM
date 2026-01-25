8c4ad614ffef5353246b30425bc021b3f2ee063e
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-25 12:40:22 +0530

    added scanf char.c

---
+++ scanf char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    char sent;
+    printf("Write a random Letter: ");
+    scanf("%c",&sent);
+    printf("The Writen Letter is: %c",sent);
+    return 0;
+}
