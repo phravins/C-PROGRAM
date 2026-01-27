e701613537fae59705e8c8908cf3267ac5c2206c
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-27 12:17:44 +0530

    Create scanf code.c

---
+++ scanf code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <Stdlib.h>
+int main () {
+    int number;
+    printf("Enter your date of birth: ");
+    scanf("%d",&number);
+    printf("Your date of birth is: %d ",number);
+    return 0;
+
+}
