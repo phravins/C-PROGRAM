5be838b8a14e67b1dfee014213860f6629b7ad46
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-01-28 12:03:17 +0530

    Create Arithmetic user IP.c

---
+++ Arithmetic user IP.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () {
+    int num1, num2;
+    printf("ENTER NUMBER 1: ");
+    scanf("%d",&num1);
+
+    printf("ENTER NUMBER 2: ");
+    scanf("%d",&num2);
+
+    printf("The addition of the given numbers : %d",num1 + num2);
+    return 0;
+}
+
