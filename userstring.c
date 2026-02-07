e26d541e524049d61d8e4238889a8bf79cf8f7ec
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-02-07 20:36:39 +0530

    Create userstring.c

---
+++ userstring.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  char firstName[30];
+  printf("Enter your first name and press enter: \n");
+  scanf("%s", firstName);
+  printf("Hello %s", firstName);
+  
+  return 0;
+}
