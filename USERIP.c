2d9f1136a2dc8db351867792d3c2f38ff9decfc8
PHRAVIN S <164343676+phravins@users.noreply.github.com>
2026-02-27 12:24:27 +0530

    Add USERIP.c to read number and character input

---
+++ USERIP.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int mynum;
+    char mychar;
+    printf("ENTER THE NUMBER AND CHARACTER ADN PRESS ENTER: ");
+    scanf("%d %c",&mynum,&mychar);
+    printf("YOUR NUMBER IS: %d\n",mynum);
+    printf("YOUR CHARACTER IS: %c",mychar);
+    return 0;
+}
